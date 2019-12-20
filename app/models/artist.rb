6d99907d53c4c79c35a33c6ab26de14adb77e5a3
Alouiscious Media <info@reneesoul.com>
2019-12-20 10:52:06 -0800

    all but one test passing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,12 @@
 class Artist < ActiveRecord::Base
   has_many :songs
   
+  def song_title
+    Artist.song.title
+  end
+
+
+  def song_count
+    self.songs.size
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  # def song_count
+  #   Artist.songs.count
+  # end
+
+  def artist_name
+    self.artist.name
+  end
 end
